7dcc45ec3a3b1720d013008a47fc73bae1926b25
Ryan Reynolds <ryan.reynolds@thetradedesk.com>
2021-06-03 15:37:25 -0700

    Add ability to write/test in scala in the feature store client

---
+++ mlplatform/fsclient/src/test/scala/ExampleTest.scala
@@ -0,0 +1,16 @@
+import collection.mutable.Stack
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+
+class ExampleSpec extends AnyFlatSpec with Matchers {
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    stack.pop() should be (2)
+    stack.pop() should be (1)
+  }
+
+}
