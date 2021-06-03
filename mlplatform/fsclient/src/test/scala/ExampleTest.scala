8aee58b94c83b57951283fe2cde33bc4dbacaf1a
Ryan Reynolds <ryan.reynolds@thetradedesk.com>
2021-06-03 15:47:51 -0700

    Test FeatureStoreClient in scala

--- mlplatform/fsclient/src/test/scala/ExampleTest.scala
+++ mlplatform/fsclient/src/test/scala/ExampleTest.scala
@@ -1,16 +1,32 @@
+import com.thetradedesk.mlplatform.fsclient.{FeatureStoreClient, FeatureStoreClientConfig}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.scalatest.Ignore
+
 import collection.mutable.Stack
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
+@Ignore
 class ExampleSpec extends AnyFlatSpec with Matchers {
 
-  "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    stack.pop() should be (2)
-    stack.pop() should be (1)
+  "Test run" should "not throw an error" in {
+
+    val conf = new SparkConf().setAppName("TestFSClient").setMaster("local")
+    val sc = new SparkContext(conf)
+    val spark = SparkSession.builder.config(conf).getOrCreate()
+
+
+    val clientConfig = new FeatureStoreClientConfig
+    val client = new FeatureStoreClient(clientConfig)
+
+    val ds = client.ReadFeature(spark, 1L, 1L, 1L)
+
+    ds.printSchema()
+
+
   }
+
 
 }
