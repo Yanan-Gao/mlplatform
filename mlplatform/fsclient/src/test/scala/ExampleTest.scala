1292e563be174cf7a94af4dfd38ecd1f546823b0
Ryan Reynolds <ryan.reynolds@thetradedesk.com>
2021-06-04 14:47:58 -0700

    remove scala testing while testing maven builds in gitlab

--- mlplatform/fsclient/src/test/scala/ExampleTest.scala
+++ mlplatform/fsclient/src/test/scala/ExampleTest.scala
@@ -1,16 +1,10 @@
-import com.thetradedesk.mlplatform.fsclient.{FeatureStoreClient, FeatureStoreClientConfig}
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.scalatest.Ignore
-
-import collection.mutable.Stack
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
-import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 @Ignore
 class ExampleSpec extends AnyFlatSpec with Matchers {
-
+/*
   "Test run" should "not throw an error" in {
 
     val conf = new SparkConf().setAppName("TestFSClient").setMaster("local")
@@ -27,6 +21,6 @@ class ExampleSpec extends AnyFlatSpec with Matchers {
 
 
   }
-
+*/
 
 }
