d6119ecb699a214a368b4bfc6a6222350f5a2abc
Dien Nguyen <dien.nguyen@thetradedesk.com>
2022-05-02 19:41:12 +0000

    Dpn audauto 302 fix cicd release

--- kongming/src/main/bash/clustersetup.sh
+++ kongming/src/main/bash/clustersetup.sh
@@ -25,7 +25,7 @@ sudo yum clean expire-cache
 echo "restarting docker! \n"
 sudo systemctl restart docker
 
-echo "nvidia docker tool set up complete \n starting s3 sync.. \n"
+echo "nvidia docker tool set up complete \n starting s3 cp.. \n"
 
 
 echo "starting training data transfer"
@@ -43,37 +43,24 @@ fi
 
 BASE_S3_PATH="s3://thetradedesk-mlplatform-us-east-1/data/${ENV}"
 MODEL_INPUT="kongming/trainset/v=1"
-#META_DATA="metadata/google"
+#META_DATA="metadata"
 DATE_PATH="date=${DATE_PARTITION}"
 
 TRAINING_DATA_SOURCE="${BASE_S3_PATH}/${MODEL_INPUT}/${DATE_PATH}/split=train_tfrecord/"
 VALIDATION_DATA_SOURCE="${BASE_S3_PATH}/${MODEL_INPUT}/${DATE_PATH}/split=val_tfrecord/"
 
-# todo: hard code right now since we don't generate it yet
+# todo: Chuanxi: we don't generate metadata and don't need it for now
 #META_SOURCE="${BASE_S3_PATH}/${META_DATA}/${DATE_PATH}/"
-META_SOURCE="s3://thetradedesk-useast-hadoop/cxw/share/kongming/assets_string/"
+#META_SOURCE="s3://thetradedesk-useast-hadoop/cxw/share/kongming/assets_string/"
 
 
-TRAINING_DATA_DEST="file:/mnt/input/train"
-VALIDATION_DATA_DEST="file:/mnt/input/val"
-META_DEST="file:/mnt/assets_string/"
+TRAINING_DATA_DEST="/mnt/input/train"
+VALIDATION_DATA_DEST="/mnt/input/val"
+#META_DEST="file:/mnt/assets_string/"
 
-mkdir -p TRAINING_DATA_DEST
-mkdir -p VALIDATION_DATA_DEST
-mkdir -p META_DEST
-
-# todo: is s3distcp actually better?
-s3-dist-cp --src=${TRAINING_DATA_SOURCE} --dest=${TRAINING_DATA_DEST} --srcPattern=.*tfrecord.gz
-s3-dist-cp --src=${VALIDATION_DATA_SOURCE} --dest=${VALIDATION_DATA_DEST} --srcPattern=.*tfrecord.gz
-s3-dist-cp --src=${META_SOURCE} --dest=${META_DEST}
-
-
-#echo "syncing from ${DATA_SOURCE} to ${SYNC_DEST}"
-#cd ${MNT} && aws s3 sync ${DATA_SOURCE} ${SYNC_DEST} --quiet
-#
-#echo "syncing meta data form ${META_SOURCE} to ${META_DEST}"
-#aws s3 sync ${META_SOURCE} ${META_DEST} --quiet
+aws s3 cp ${TRAINING_DATA_SOURCE} ${TRAINING_DATA_DEST} --recursive --include "*.tfrecord.gz"
+aws s3 cp ${VALIDATION_DATA_SOURCE} ${VALIDATION_DATA_DEST} --recursive --include "*.tfrecord.gz"
+#s3-dist-cp --src=${META_SOURCE} --dest=${META_DEST}
 
 echo "set up complete"
 
-# ./clustersetup.sh dev 20220423
--- kongming/src/main/bash/runmodel.sh
+++ kongming/src/main/bash/runmodel.sh
@@ -14,9 +14,8 @@ fi
 
 MODEL_INPUT="/opt/application/"
 DOCKER_IMAGE_NAME="kongming/training-gpu"
-DOCKER_IMAGE_VERSION="0.2"
-#DOCKER_INTERNAL_BASE="internal.docker.adsrvr.org"
-DOCKER_INTERNAL_BASE="dev.docker.adsrvr.org"
+DOCKER_IMAGE_VERSION="release"
+DOCKER_INTERNAL_BASE="internal.docker.adsrvr.org"
 DOCKER_USER="svc.emr-docker-ro"
 HOME_HADOOP="/mnt"
 
@@ -32,24 +31,8 @@ eval docker login -u $DOCKER_USER -p $CREDS $DOCKER_INTERNAL_BASE
 
 eval docker pull ${DOCKER_INTERNAL_BASE}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}
 
-
-#sudo docker run --gpus all --shm-size=5g --ulimit memlock=-1 -v /mnt/tfrecords:${MODEL_INPUT}/tfrecords -v /mnt/metadata:${MODEL_INPUT}/metadata/ \
-#  ${DOCKER_INTERNAL_BASE}/${DOCKER_IMAGE_NAME}:release  \
-#      "--nodummy" \
-#      "--batch_size=8192" \
-#      "--eval_batch_size=197934" \
-#      "--num_epochs=65" \
-#      "--steps_per_epoch=3000" \
-#      "--input_path=/var/tmp/input/tfrecords/" \
-#      "--meta_data_path=/var/tmp/input/metadata" \
-#      "--exclude_features=ImpressionPlacementId,AdFormat,DealId" \
-#      "--training_verbosity=2" \
-#      "--model_arch=dlrm" \
-#      "--early_stopping_patience=5"
-
 sudo docker run --gpus all \
            -v ${INPUT_DEST}:/opt/application/input/ \
-           -v ${META_DEST}:/opt/application/assets_string/ \
            ${DOCKER_INTERNAL_BASE}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} \
       "--env=${ENV}" \
       "--model_creation_date=${DATE_PARTITION}" \
@@ -59,4 +42,3 @@ sudo docker run --gpus all \
       "--model_choice=basic" \
       "--early_stopping_patience=5"
 
-# ./runmodel.sh dev 20220423
