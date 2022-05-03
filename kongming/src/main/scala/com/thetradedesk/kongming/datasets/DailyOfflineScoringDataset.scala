d6c610dda23df1749da137c5c60970cfea2152f1
Chuanxi Wang <chuanxi.wang@thetradedesk.com>
2022-05-03 06:49:17 +0000

    Cxw audauto 352 scoring data set

---
+++ kongming/src/main/scala/com/thetradedesk/kongming/datasets/DailyOfflineScoringDataset.scala
@@ -0,0 +1,41 @@
+package com.thetradedesk.kongming.datasets
+
+//TODO: will need to be able to extend what Yuehan has as final result for trainingset.
+final case class DailyOfflineScoringRecord(
+                                           AdGroupId: Int,
+                                           BidRequestIdStr: String,
+                                           AdGroupIdStr: String,
+                                           SupplyVendor: Option[Int],
+                                           SupplyVendorPublisherId: Option[Int],
+                                           Site: Option[Int],
+                                           AdFormat: Int,
+
+                                           Country: Option[Int],
+                                           Region: Option[Int],
+                                           City: Option[Int],
+                                           Zip: Option[Int],
+
+                                           DeviceMake: Option[Int],
+                                           DeviceModel: Option[Int],
+                                           RequestLanguages: Int,
+
+                                           RenderingContext: Option[Int],
+                                           DeviceType: Option[Int],
+                                           OperatingSystem: Option[Int],
+                                           Browser: Option[Int],
+                                           MatchedFoldPosition: Int,
+
+                                           sin_hour_week: Double,
+                                           cos_hour_week: Double,
+                                           sin_hour_day: Double,
+                                           cos_hour_day: Double,
+                                           sin_minute_hour: Double,
+                                           cos_minute_hour: Double,
+                                           Latitude: Option[Double],
+                                           Longitude: Option[Double]
+                                      )
+
+object DailyOfflineScoringDataset extends KongMingDataset[DailyOfflineScoringRecord](
+  s3DatasetPath = "dailyofflinescore/v=1",
+  defaultNumPartitions = 100
+)
---
+++ kongming/src/main/scala/job/DailyOfflineScoringSet.scala
@@ -0,0 +1,52 @@
+package job
+
+import com.thetradedesk.geronimo.bidsimpression.schema.{BidsImpressions, BidsImpressionsSchema}
+import com.thetradedesk.geronimo.shared.{GERONIMO_DATA_SOURCE, loadParquetData, FLOAT_FEATURE_TYPE, INT_FEATURE_TYPE, STRING_FEATURE_TYPE}
+import com.thetradedesk.kongming._
+import com.thetradedesk.kongming.datasets.{AdGroupPolicyDataset, DailyOfflineScoringDataset}
+import com.thetradedesk.spark.TTDSparkContext.spark
+import com.thetradedesk.spark.TTDSparkContext.spark.implicits._
+import com.thetradedesk.spark.util.prometheus.PrometheusClient
+import com.thetradedesk.geronimo.shared.schemas.ModelFeature
+import com.thetradedesk.geronimo.shared.intModelFeaturesCols
+import com.thetradedesk.kongming.transform.OfflineScoringSetTransform
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.Column
+import job.GenerateTrainSet.modelFeatures
+
+import java.time.LocalDate
+
+object DailyOfflineScoringSet {
+
+  val keptFields: Array[ModelFeature] = Array(
+    ModelFeature("BidRequestId", STRING_FEATURE_TYPE, None, 0),
+    ModelFeature("AdGroupId", STRING_FEATURE_TYPE, None, 0),
+  )
+
+  def modelKeepFeatureCols(features: Seq[ModelFeature]): Array[Column] = {
+    features.map(f => col(f.name).alias(f.name+"Str")).toArray
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val prometheus = new PrometheusClient("KoaV4Conversion", "DailyOfflineScoringSet")
+
+    val bidImpressionsS3Path = BidsImpressions.BIDSIMPRESSIONSS3 + "prod/bidsimpressions/"
+    val bidsImpressions = loadParquetData[BidsImpressionsSchema](bidImpressionsS3Path, date, source = Some(GERONIMO_DATA_SOURCE))
+
+    val adGroupPolicyHardCodedDate = LocalDate.parse("2022-03-15")
+    val adGroupPolicy = AdGroupPolicyDataset.readHardCodedDataset(adGroupPolicyHardCodedDate)
+
+    val selectionTabular = intModelFeaturesCols(modelFeatures) ++ modelKeepFeatureCols(keptFields)
+
+    val scoringFeatureDS = OfflineScoringSetTransform.dailyTransform(
+      bidsImpressions,
+      adGroupPolicy,
+      selectionTabular
+    )(prometheus)
+
+    //assuming Yuehan has implemented the tfrecord write this way. has dependency on the changes she is doing.
+    DailyOfflineScoringDataset.writePartition(scoringFeatureDS, date, format=Option("tfrecord"))
+
+  }
+}
