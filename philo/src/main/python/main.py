e878cbae12bba97e0b08a468de30e3aa6a30440f
Fei Gao <fei.gao@thetradedesk.com>
2023-04-27 15:32:13 +0000

    fxg-DATSCI-2916-main-for-neo

--- philo/src/main/python/main.py
+++ philo/src/main/python/main.py
@@ -1,9 +1,11 @@
 import sys
+import os
 from datetime import datetime
 
 import tensorflow as tf
 import atexit
 from absl import app, flags
+from tensorflow.keras.layers import Embedding
 
 from philo.data import prepare_dummy_data, prepare_real_data, s3_copy, get_steps_epochs_emr, TRAIN, VAL, TEST
 from philo.data import list_tfrecord_files
@@ -13,7 +15,7 @@ from philo.models import model_builder
 from philo.utils import get_callbacks
 from philo.prometheus import Prometheus
 from philo.feature_utils import get_features_from_json
-
+from philo.neo import extract_dnn_only_models
 
 FLAGS = flags.FLAGS
 
@@ -23,9 +25,12 @@ MODEL_LOGS = "/var/tmp/logs/"
 META_DATA_INPUT = "/var/tmp/input"
 LATEST_MODEL = "/var/tmp/input"
 FEATURES_PATH = "features.json"
-S3_PROD = "s3://thetradedesk-mlplatform-us-east-1/models/{env}/philo/v=1/{region}/"
+S3_PROD = "s3://thetradedesk-mlplatform-us-east-1/models/{env}/philo/v=3/{region}/"
 # PARAM_MODEL_OUTPUT = "models_params/"
 MODEL_OUTPUT = "models/"
+NEO_A_OUTPUT = "adgroup/"
+NEO_B_OUTPUT = "bidrequest/"
+BIAS_OUTPUT = "bias/"
 MODEL_REGION = "namer"
 EVAL_OUTPUT = "eval_metrics/"
 S3_MODEL_LOGS = "model_logs/"
@@ -39,8 +44,8 @@ DATE_TIME = datetime.now()
 flags.DEFINE_string('env', default='dev', help=f'Execution environment (dev, test, prod)')
 
 # define model structure
-flags.DEFINE_string("model_arch", default="deepfm", help="Model Architecture, currently only have deep fm")
-flags.DEFINE_list("dnn_hidden_units", default=[128, 128], help="Number of units in the deep tower")
+flags.DEFINE_string("model_arch", default="deepfm_dual", help="Model Architecture, dual DNN towers")
+flags.DEFINE_list("dnn_hidden_units", default=[[64, 64], [128, 64]], help="Number of units in the deep tower")
 flags.DEFINE_string('dnn_activation', default="relu", help='dnn activation function')
 flags.DEFINE_float('l2_reg_linear', default=0.00001, help='linear layer l2 regularization weights')
 flags.DEFINE_float('l2_reg_embedding', default=0.00001, help='embedding layer l2 regularization weights')
@@ -62,7 +67,8 @@ flags.DEFINE_string('s3_output_path', default=S3_PROD,
 # blank region supports existing regionless training
 flags.DEFINE_string('region', default='', help=f'Region for the model data. Default blank {MODEL_REGION}')
 
-flags.DEFINE_boolean('push_training_logs', default=False, help=f'option to push all logs to s3 for debugging. defaulted false')
+flags.DEFINE_boolean('push_training_logs', default=False,
+                     help=f'option to push all logs to s3 for debugging. defaulted false')
 
 flags.DEFINE_string('log_tag', default=f"{DATE_TIME.strftime('%Y-%m-%d-%H-%M')}", help='log tag')
 
@@ -95,7 +101,7 @@ flags.DEFINE_enum('job', 'running', ['running', 'stopped'], 'Job status.')
 
 flags.DEFINE_string('model_creation_date',
                     default=DATE_TIME.strftime("%Y%m%d%H%M"),
-                    help='Time the model was created. Its ISO date format YYYYMMDDHHMM (e.g. 2021123114) defaults now'
+                    help='Time the model was created. Its ISO date format YYYYMMDDHHMM (e.g. 202112311459) defaults now'
                          'Not related to the date on the training data, rather is used to identify when the model was '
                          'trained. Primary use is for model loader to load latest model to prod.')
 
@@ -104,15 +110,16 @@ app.parse_flags_with_usage(sys.argv)
 
 
 def main(argv):
-    model_features = get_features_from_json(FEATURES_PATH, FLAGS.exclude_features)
+    model_features, adgroup_feature_list = get_features_from_json(FEATURES_PATH, FLAGS.exclude_features,
+                                                                  get_adgroup_feature=True)
     model_target = DEFAULT_MODEL_TARGET
 
     # define training metrics
-    prom = Prometheus(job_name='philo', application='modelTraining', environment=FLAGS.env)
+    prom = Prometheus(job_name='philo_neo', application='modelTraining', environment=FLAGS.env)
     epoch_gauge = prom.define_gauge('epochs', 'number of epochs', ['region'])
-    loss_gauge = prom.define_gauge('loss', 'loss value', ['region'])
-    val_loss_gauge = prom.define_gauge('val_loss', 'validation loss', ['region'])
-    eval_philo_gauge = prom.define_gauge('eval', 'evaluation', ['region'])
+    loss_gauge = prom.define_gauge('loss', 'loss value', ['region', 'model'])
+    val_loss_gauge = prom.define_gauge('val_loss', 'validation loss', ['region', 'model'])
+    eval_philo_gauge = prom.define_gauge('eval', 'evaluation', ['region', 'model'])
 
     # if the training process need to go through the whole dataset in more than 1 epoch
     # we need to repeat the data and get the steps_per_epochs so that tf knows how the
@@ -138,7 +145,6 @@ def main(argv):
         batch_size=FLAGS.batch_size, eval_batch_size=FLAGS.eval_batch_size, prefetch_num=tf.data.AUTOTUNE,
         repeat=repeat)
 
-
     mirrored_strategy = tf.distribute.MultiWorkerMirroredStrategy()
     num_gpus = mirrored_strategy.num_replicas_in_sync
     print("##########################checking gpu devices#############################")
@@ -150,70 +156,182 @@ def main(argv):
     # close functions before Python exit
     # TODO: remove when theres a tf patch to resolve this
 
-    # Register the `_cross_device_ops._pool` and `_host_cross_device_ops._pool`s `.close()` function to run when
-    # Python exists
-    atexit.register(mirrored_strategy._extended._cross_device_ops._pool.close)
-    atexit.register(mirrored_strategy._extended._host_cross_device_ops._pool.close)
-    with mirrored_strategy.scope():
-        # currently not using the latest model, but can be used in the future
-        # try:
-        #     model = tf.keras.models.load_model(FLAGS.latest_model_path, custom_objects=custom_objects)
-        # except OSError as error:
-            # if no model file, create a new model from scratch
-            # print(error)
-        kwargs = {"dnn_hidden_units": tuple(FLAGS.dnn_hidden_units), "l2_reg_linear": FLAGS.l2_reg_linear,
-                  "l2_reg_embedding": FLAGS.l2_reg_embedding, "l2_reg_dnn": FLAGS.l2_reg_dnn,
-                  "dnn_dropout": FLAGS.dnn_dropout, "dnn_activation": FLAGS.dnn_activation,
-                  "dnn_use_bn": FLAGS.dnn_use_bn}
-        print('Number of devices: %d' % mirrored_strategy.num_replicas_in_sync)
-        model = model_builder(FLAGS.model_arch, model_features, **kwargs)
+    # # Register the `_cross_device_ops._pool` and `_host_cross_device_ops._pool`s `.close()` function to run when
+    # # Python exists
+    # atexit.register(mirrored_strategy._extended._cross_device_ops._pool.close)
+    # atexit.register(mirrored_strategy._extended._host_cross_device_ops._pool.close)
+    # with mirrored_strategy.scope():
+    #     # currently not using the latest model, but can be used in the future
+    #     # try:
+    #     #     model = tf.keras.models.load_model(FLAGS.latest_model_path, custom_objects=custom_objects)
+    #     # except OSError as error:
+    #     # if no model file, create a new model from scratch
+    #     # print(error)
+    #     kwargs = {"dnn_hidden_units": tuple(FLAGS.dnn_hidden_units), "l2_reg_linear": FLAGS.l2_reg_linear,
+    #               "l2_reg_embedding": FLAGS.l2_reg_embedding, "l2_reg_dnn": FLAGS.l2_reg_dnn,
+    #               "dnn_dropout": FLAGS.dnn_dropout, "dnn_activation": FLAGS.dnn_activation,
+    #               "dnn_use_bn": FLAGS.dnn_use_bn, "adgroup_feature_list": adgroup_feature_list}
+    #     print('Number of devices: %d' % mirrored_strategy.num_replicas_in_sync)
+    #     model_1 = model_builder(FLAGS.model_arch, model_features, **kwargs)
+    #
+    #     auc = tf.keras.metrics.AUC()
+    # TODO: include later with multi gpu training
 
-        auc = tf.keras.metrics.AUC()
-    print("##########################model summary###################################")
-    model.summary()
+    kwargs = {"dnn_hidden_units": tuple(FLAGS.dnn_hidden_units), "l2_reg_linear": FLAGS.l2_reg_linear,
+              "l2_reg_embedding": FLAGS.l2_reg_embedding, "l2_reg_dnn": FLAGS.l2_reg_dnn,
+              "dnn_dropout": FLAGS.dnn_dropout, "dnn_activation": FLAGS.dnn_activation,
+              "dnn_use_bn": FLAGS.dnn_use_bn, "adgroup_feature_list": adgroup_feature_list}
+    print('Number of devices: %d' % mirrored_strategy.num_replicas_in_sync)
+    model_1 = model_builder(FLAGS.model_arch, model_features, **kwargs)
 
-    model.compile(tf.keras.optimizers.Adam(learning_rate=FLAGS.learning_rate),
-                  "binary_crossentropy",
-                  # when incremental training we need to be careful about how auc gets instantiated
-                  metrics=['binary_crossentropy', auc])
+    auc = tf.keras.metrics.AUC()
 
-    checkpoint_base_path = f"{FLAGS.output_path}checkpoints/"
-    print("##########################start model training#############################")
+    # step 1: train a deepfm_dual model, which has the dual DNN tower structure
+    # this step is the same as the original philo alpha
+    print("##########################step 1: model summary###################################")
+    model_1.summary()
 
-    history = model.fit(datasets[TRAIN],
-                        epochs=epochs,
-                        steps_per_epoch=steps_per_epoch,
-                        verbose=FLAGS.training_verbosity,
-                        callbacks=get_callbacks(FLAGS.log_path, FLAGS.profile_batches,
-                                                FLAGS.early_stopping_patience, checkpoint_base_path),
-                        validation_data=datasets[VAL],
-                        validation_steps=None,
-                        validation_batch_size=FLAGS.eval_batch_size)
-    print("##########################end of model training############################")
+    model_1.compile(tf.keras.optimizers.Adam(learning_rate=FLAGS.learning_rate),
+                    "binary_crossentropy",
+                    # when incremental training we need to be careful about how auc gets instantiated
+                    metrics=['binary_crossentropy', auc])
 
+    checkpoint_base_path_1 = f"{FLAGS.output_path}/step_1/checkpoints/"
+    print("##########################step 1: start model training#############################")
+    history_1 = model_1.fit(datasets[TRAIN],
+                            epochs=epochs,
+                            steps_per_epoch=steps_per_epoch,
+                            verbose=FLAGS.training_verbosity,
+                            callbacks=get_callbacks(f"{FLAGS.log_path}step_1/", FLAGS.profile_batches,
+                                                    FLAGS.early_stopping_patience, checkpoint_base_path_1),
+                            validation_data=datasets[VAL],
+                            validation_steps=None,
+                            validation_batch_size=FLAGS.eval_batch_size)
+    print("##########################step 1: end of model training############################")
 
-    model_tag = f"{FLAGS.output_path}model/{FLAGS.model_arch}_{FLAGS.num_epochs}_{FLAGS.dnn_dropout}_{FLAGS.dnn_use_bn}"
-    print("##########################saving model#####################################")
-    model.save(model_tag)
+    model_tag_1 = f"{FLAGS.output_path}model/step_1/{FLAGS.model_arch}_{FLAGS.num_epochs}_{FLAGS.dnn_dropout}_{FLAGS.dnn_use_bn}"
+
+    print("##########################step 1: saving model#####################################")
+    model_1.save(model_tag_1)
+
+    # step 2: freezing the embeddings trained, retrain the model with only dual DNN towers
+    # this step ensures that the DNN part will learn the embeddings to predict, unfreezing the whole model will lead to
+    # the model struggling ~ 0.5 AUC
+    model_2, model_neo_a, model_neo_b = extract_dnn_only_models(model_1)
+
+    # freeze embedding layers
+    for layer in list(filter(lambda x: isinstance(x, Embedding), model_2.layers)):
+        layer.trainable = False
+
+    print("##########################step 2: model summary###################################")
+    model_2.summary()
+
+    model_2.compile(tf.keras.optimizers.Adam(learning_rate=FLAGS.learning_rate),
+                    "binary_crossentropy",
+                    # when incremental training we need to be careful about how auc gets instantiated
+                    metrics=['binary_crossentropy', auc])
+
+    checkpoint_base_path_2 = f"{FLAGS.output_path}/step_2/checkpoints/"
+
+    print("##########################step 2: start model training#############################")
+    _ = model_2.fit(datasets[TRAIN],
+                    epochs=epochs,
+                    steps_per_epoch=steps_per_epoch,
+                    verbose=FLAGS.training_verbosity,
+                    callbacks=get_callbacks(f"{FLAGS.log_path}step_2/", FLAGS.profile_batches,
+                                            FLAGS.early_stopping_patience, checkpoint_base_path_2),
+                    validation_data=datasets[VAL],
+                    validation_steps=None,
+                    validation_batch_size=FLAGS.eval_batch_size)
+    print("##########################step 2: end of model training############################")
+
+    model_tag_2 = f"{FLAGS.output_path}model/step_2/{FLAGS.model_arch}_{FLAGS.num_epochs}_{FLAGS.dnn_dropout}_{FLAGS.dnn_use_bn}"
+    print("##########################step 2: saving model#####################################")
+    model_2.save(model_tag_2)
+
+    # step 3: unfreezing the DNN only model trained, fine tune the model to improve performance
+    # in this step, a smaller learning rate of 1/10 of the origincal will be used
+
+    # unfreeze the DNN only model
+    model_2.trainable = True
+
+    print("##########################step 3: model summaries###################################")
+    model_2.summary()
+    model_neo_a.summary()
+    model_neo_b.summary()
+
+    model_2.compile(tf.keras.optimizers.Adam(learning_rate=FLAGS.learning_rate / 10),  # much lower learning rate
+                    "binary_crossentropy",
+                    # when incremental training we need to be careful about how auc gets instantiated
+                    metrics=['binary_crossentropy', auc])
+
+    checkpoint_base_path_3 = f"{FLAGS.output_path}/step_3/checkpoints/"
+
+    print("##########################step 3: start model training#############################")
+    history_3 = model_2.fit(datasets[TRAIN],
+                            epochs=epochs,
+                            steps_per_epoch=steps_per_epoch,
+                            verbose=FLAGS.training_verbosity,
+                            callbacks=get_callbacks(f"{FLAGS.log_path}step_3/", FLAGS.profile_batches,
+                                                    FLAGS.early_stopping_patience, checkpoint_base_path_3),
+                            validation_data=datasets[VAL],
+                            validation_steps=None,
+                            validation_batch_size=FLAGS.eval_batch_size)
+    print("##########################step 3: end of model training############################")
+
+    model_tag_3_full = f"{FLAGS.output_path}model/step_3/full/{FLAGS.model_arch}_{FLAGS.num_epochs}_{FLAGS.dnn_dropout}_{FLAGS.dnn_use_bn}"
+    model_tag_3_neo_a = f"{FLAGS.output_path}model/step_3/neo_a/{FLAGS.model_arch}_{FLAGS.num_epochs}_{FLAGS.dnn_dropout}_{FLAGS.dnn_use_bn}_neo_a"
+    model_tag_3_neo_b = f"{FLAGS.output_path}model/step_3/neo_b/{FLAGS.model_arch}_{FLAGS.num_epochs}_{FLAGS.dnn_dropout}_{FLAGS.dnn_use_bn}_neo_b"
+    print("##########################step 3: saving models#####################################")
+    model_2.save(model_tag_3_full)
+    model_neo_a.save(model_tag_3_neo_a)
+    model_neo_b.save(model_tag_3_neo_b)
+
+    # save the weight of the prediction layer of the final tuned model
+    # this serves as the bias term added to the product of Neo outputs, which altogether will feed to the sigmoid
+    # function for the final prediction of probability
+    bias_term = model_2.layers[-1].get_weights()[0][0]
+    bias_tag = f"{FLAGS.output_path}model/step_3/bias/{FLAGS.model_arch}_{FLAGS.num_epochs}_{FLAGS.dnn_dropout}_{FLAGS.dnn_use_bn}.csv"
+    os.makedirs(os.path.dirname(bias_tag), exist_ok=True)
+    with open(bias_tag, 'w') as f:
+        f.write(str(bias_term))
+
+    # save to s3
     base_s3_path = FLAGS.s3_output_path.format(env=FLAGS.env, region=FLAGS.region)
 
     # push logs if needed
     if FLAGS.push_training_logs:
         path = f"{base_s3_path}{S3_MODEL_LOGS}{FLAGS.model_creation_date}"
-        print(f"##########################Writing logs to {path}...#######################")
+        print(f"##########################writing logs to {path}...#######################")
         s3_copy(FLAGS.log_path, path)
 
-    # copy model output to final location
-    path = f"{base_s3_path}{MODEL_OUTPUT}{FLAGS.model_creation_date}"
-    print(f"##########################Writing model to {path}...######################")
-    s3_copy(model_tag, path)
+    # copy model outputs to final location
+    print(f"##########################writing models to {base_s3_path}...######################")
+    path_philo = f"{base_s3_path}{MODEL_OUTPUT}{FLAGS.model_creation_date}"
+    path_neo_a = f"{base_s3_path}{NEO_A_OUTPUT}{FLAGS.model_creation_date}"
+    path_neo_b = f"{base_s3_path}{NEO_B_OUTPUT}{FLAGS.model_creation_date}"
+    path_bias = f"{base_s3_path}{BIAS_OUTPUT}{FLAGS.model_creation_date}"
+    print(f"Writing Philo model to {path_philo}...")
+    s3_copy(f"{FLAGS.output_path}model/step_1", path_philo)
+    print(f"Writing Neo AdGroup model to {path_neo_a}...")
+    s3_copy(f"{FLAGS.output_path}model/step_3/neo_a", path_neo_a)
+    print(f"Writing Neo BidRequest model to {path_neo_b}...")
+    s3_copy(f"{FLAGS.output_path}model/step_3/neo_b", path_neo_b)
+    print(f"Writing bias term to {path_bias}...")
+    s3_copy(f"{FLAGS.output_path}model/step_3/bias", path_bias)
 
+    print("##########################logging model metrics########################")
     epoch_gauge.labels(region=FLAGS.region).set(epochs)
-    loss_gauge.labels(region=FLAGS.region).set(history.history['loss'][-1])
-    val_loss_gauge.labels(region=FLAGS.region).set(history.history['val_loss'][-1])
+    loss_gauge.labels(region=FLAGS.region, model="philo").set(history_1.history['loss'][-1])
+    loss_gauge.labels(region=FLAGS.region, model="neo").set(history_3.history['loss'][-1])
+    val_loss_gauge.labels(region=FLAGS.region, model="philo").set(history_1.history['val_loss'][-1])
+    val_loss_gauge.labels(region=FLAGS.region, model="neo").set(history_3.history['val_loss'][-1])
+
     print("##########################evaluating model on TEST########################")
-    evals = model.evaluate(datasets[TEST], verbose=FLAGS.training_verbosity)
-    eval_philo_gauge.labels(region=FLAGS.region).set(evals[2])
+    evals_1 = model_1.evaluate(datasets[TEST], verbose=1)
+    evals_3 = model_2.evaluate(datasets[TEST], verbose=1)
+    eval_philo_gauge.labels(region=FLAGS.region, model="philo").set(evals_1[2])
+    eval_philo_gauge.labels(region=FLAGS.region, model="neo").set(evals_3[2])
 
     # push metrics before stopping the job
     prom.push()
