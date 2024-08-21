faeecfac2afcaafe7f804d8ed04e2e4ce7faf2fd
Adithya Badidey <adithya.badidey@thetradedesk.com>
2024-08-21 20:42:50 +0000

    abm-DATPERF-5099-use-latest-afgroupbudget-snapshot

--- plutus/src/main/scala/com/thetradedesk/plutus/data/schema/ProductionAdgroupBudgetDataset.scala
+++ plutus/src/main/scala/com/thetradedesk/plutus/data/schema/ProductionAdgroupBudgetDataset.scala
@@ -1,16 +1,14 @@
 package com.thetradedesk.plutus.data.schema
 
-import com.thetradedesk.plutus.data.paddedDatePart
-import com.thetradedesk.spark.datasets.core.S3Roots
+import com.thetradedesk.spark.datasets.core.SchemaPolicy.StrictCaseClassSchema
+import com.thetradedesk.spark.datasets.core.VerticaBackupsS3DataSet
 
-import java.time.LocalDateTime
-
-object ProductionAdgroupBudgetDataset {
-  val S3PATH = f"${S3Roots.VERTICA_EXPORT_ROOT}/ExportProductionAdGroupBudgetSnapshot/VerticaAws/"
-  def S3PATH_GEN = (dateTime: LocalDateTime) => {
-    f"date=${paddedDatePart(dateTime.toLocalDate)}/hour=${dateTime.getHour}%02d"
-  }
-}
+case class ProductionAdgroupBudgetDataset()
+  extends VerticaBackupsS3DataSet[ProductionAdgroupBudgetData](
+    "ExportLateDataPlatformReport/VerticaAws",
+    timestampFieldName = "ReportHourUtc",
+    schemaPolicy = StrictCaseClassSchema
+  ) {}
 
 final case class ProductionAdgroupBudgetData(
                                           AdGroupId: String,
