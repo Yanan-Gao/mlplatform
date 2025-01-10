c9bcd1e83553781b22d2b83563bf0a0227f90a59
Yang Lin <yang.lin@thetradedesk.com>
2025-01-10 09:04:02 +0000

    Yll audauto 2851 prepare online measurement data

--- audience/src/main/scala/com/thetradedesk/audience/datasets/S3Roots.scala
+++ audience/src/main/scala/com/thetradedesk/audience/datasets/S3Roots.scala
@@ -9,5 +9,6 @@ object S3Roots {
   val QUBOLE_ADHOC_ROOT: String = "s3a://thetradedesk-useast-qubole/warehouse.external/thetradedesk.db/adhoc"
   val QUBOLE_VERTICA_ROOT: String = "s3a://thetradedesk-useast-qubole/warehouse.external/thetradedesk.db/vertica"
   val ML_PLATFORM_ROOT: String = "s3a://thetradedesk-mlplatform-us-east-1/data"
+  val FEATURE_STORE_ROOT: String = "s3a://thetradedesk-mlplatform-us-east-1/features/feature_store"
   val LOGS_ROOT: String = "s3a://thetradedesk-useast-logs-2"
 }
