da0093e3f80b1ce96cc998f7bb68e1f23c5af536
Yang Lin <yang.lin@thetradedesk.com>
2025-04-25 08:48:05 +0000

    Yll audauto 3303 update training data status for non array

--- audience/src/main/scala/com/thetradedesk/audience/utils/DataOperation.scala
+++ audience/src/main/scala/com/thetradedesk/audience/utils/DataOperation.scala
@@ -1,18 +1,34 @@
 package com.thetradedesk.audience.utils
 
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.{arrays_zip, col, explode}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 
 object DataFrameUtils {
 
   def explodeColumns(df: DataFrame, columns: Seq[String]): DataFrame = {
+
+    val schema = df.schema
+
+    // Convert non-array columns to arrays
+    val dfWithArrays = columns.foldLeft(df) { (tempDf, colName) =>
+      if (schema(colName).dataType.isInstanceOf[ArrayType]) {
+        tempDf
+      } else {
+        tempDf.withColumn(colName, array(col(colName)))
+      }
+    }
+
     val zippedExpr = arrays_zip(columns.map(col): _*)
-    val dfZipped = df.withColumn("zipped", zippedExpr)
+    val dfZipped = dfWithArrays.withColumn("zipped", zippedExpr)
     val dfExploded = dfZipped.withColumn("exploded", explode(col("zipped")))
+
+    // Extract values
     val dfWithCols = columns.foldLeft(dfExploded) { (tempDf, colName) =>
       tempDf.withColumn(colName, col(s"exploded.$colName"))
     }
+
+    // Drop intermediate columns
     dfWithCols.drop("zipped", "exploded")
   }
-
 }
