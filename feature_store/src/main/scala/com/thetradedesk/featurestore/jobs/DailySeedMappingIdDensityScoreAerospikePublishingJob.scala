84bc5f68fac01164bf8716bc5bebca622cb15b22
Leslie Jiang <leslie.jiang@thetradedesk.com>
2025-06-16 04:50:39 +0000

    lsj-AUDAUTO-3519-delete-old-records-from-aerospike

--- feature_store/src/main/scala/com/thetradedesk/featurestore/jobs/DailySeedMappingIdDensityScoreAerospikePublishingJob.scala
+++ feature_store/src/main/scala/com/thetradedesk/featurestore/jobs/DailySeedMappingIdDensityScoreAerospikePublishingJob.scala
@@ -10,11 +10,12 @@ import com.thetradedesk.spark.util.TTDConfig.config
 import org.apache.spark.sql.functions._
 import com.aerospike.client._
 import com.aerospike.client.async.{EventPolicy, NettyEventLoops, Throttles}
-import com.aerospike.client.listener.WriteListener
+import com.aerospike.client.listener.{DeleteListener, WriteListener}
 import com.aerospike.client.policy._
 import com.thetradedesk.spark.util.io.FSUtils
 import com.thetradedesk.spark.util.prometheus.PrometheusClient
 import io.netty.channel.nio.NioEventLoopGroup
+import org.apache.spark.sql.{DataFrame, Dataset}
 
 import java.net.{InetSocketAddress, Socket}
 import java.util.concurrent.atomic.AtomicLong
@@ -30,9 +31,15 @@ object DailySeedMappingIdDensityScoreAerospikePublishingJob extends DensityFeatu
     s"$MLPlatformS3Root/${ttdEnv}/seed_density_feature/v=1/${version}/_SUCCESS_$dc"
   }
 
+  private def getCurrentFilePath() = {
+    s"$MLPlatformS3Root/${ttdEnv}/seed_density_feature_meta/v=1/_CURRENT"
+  }
+
   override def runTransform(args: Array[String]): Unit = {
     val prometheus = new PrometheusClient("FeatureStore", "DailySeedMappingIdDensityScoreAerospikePublishingJob")
     val metricsAerospikeWriteGauge = prometheus.createGauge("perf_auto_seed_density_score_aerospike_write_result", "Aerospike write result count by status", "status")
+    val metricsAerospikeDeleteGauge = prometheus.createGauge("perf_auto_seed_density_score_aerospike_delete_result", "Aerospike delete result count by status", "status")
+
 
     // spark configuration
     spark.conf.set("spark.sql.orc.impl", "native")
@@ -45,6 +52,8 @@ object DailySeedMappingIdDensityScoreAerospikePublishingJob extends DensityFeatu
     val aerospikeMaxConcurrencyPerTask = config.getInt("aerospikeMaxConcurrencyPerTask", 16)
     val numEventLoops = config.getInt("numEventLoops", 1)
     val dc = config.getStringRequired("aerospikeDc")
+    val maxDeletionPercentage = config.getDouble("maxDeletionPercentage", 0.5)
+    val deletionWindowDays = config.getInt("deletionWindowDays", 7)
 
     val featureVersion = s"${getDateStr(date)}00"
 
@@ -73,12 +82,125 @@ object DailySeedMappingIdDensityScoreAerospikePublishingJob extends DensityFeatu
     // read source datasets
     val seedMappingIdDensityScores = readCBufferFormattedSeedDensity(featureVersion)
       .as[SeedDensityFeature]
+      .cache()
 
-    val writeResults = seedMappingIdDensityScores
+    val keysToDelete = getKeysToDelete(featureVersion, deletionWindowDays, maxDeletionPercentage)
+
+    // write today's records
+    val writeResults = writeNewRecordsToAerospike(
+      seedMappingIdDensityScores,
+      featureVersion,
+      aerospikeSeedHost,
+      aerospikePort,
+      ttl,
+      aerospikeMaxConcurrencyPerTask,
+      numEventLoops,
+      aerospikeSecrets,
+      aerospikeNamespace,
+      aerospikeSet
+    )
+
+    var successFileContent = s"writeSuccessCount:${writeResults._1} writeFailCount:${writeResults._2}"
+
+    // delete the previous keys that are not in today's keys
+    if (keysToDelete != null) {
+      val deleteResults = deleteOldRecordsFromAerospike(
+        keysToDelete,
+        aerospikeSeedHost,
+        aerospikePort,
+        ttl,
+        aerospikeMaxConcurrencyPerTask,
+        numEventLoops,
+        aerospikeSecrets,
+        aerospikeNamespace,
+        aerospikeSet
+      )
+
+      metricsAerospikeDeleteGauge.labels("success").set(deleteResults._1)
+      metricsAerospikeDeleteGauge.labels("fail").set(deleteResults._2)
+      metricsAerospikeDeleteGauge.labels("deleted").set(deleteResults._3)
+
+      successFileContent = s"$successFileContent deleteSuccessCount:${deleteResults._1} deleteFailCount:${deleteResults._2} deletedCount:${deleteResults._3}"
+    }
+
+    FSUtils.writeStringToFile(successFilePath, successFileContent)
+
+    metricsAerospikeWriteGauge.labels("success").set(writeResults._1)
+    metricsAerospikeWriteGauge.labels("fail").set(writeResults._2)
+
+    prometheus.pushMetrics()
+  }
+
+  private def getKeysToDelete(
+                               currentFeatureVersion: String,
+                               deletionWindowDays: Int,
+                               maxDeletionPercentage: Double
+                             ): Dataset[String] = {
+    try {
+      val featureVersions = FSUtils.readStringFromFile(getCurrentFilePath())
+        .split("\\r?\\n")
+        .map(_.trim)
+        .filter(v => v.length > 0 && v <= currentFeatureVersion)
+        .sortWith(_ > _)
+        .take(deletionWindowDays)
+
+      if (featureVersions.length < deletionWindowDays) {
+        Logger.log(s"not enough version in current file to run a deletion, version count: ${featureVersions.length}")
+        return null
+      }
+
+      // pick to the oldest version to delete from
+      val versionToScan = featureVersions.last
+      val featureKeysToScan = readCBufferFormattedSeedDensity(versionToScan)
+        .select($"FeatureKeyValueHashed")
+        .cache()
+
+      val featureKeysToCompareWith = featureVersions
+        .filter(v => v != versionToScan)
+        .map(v => readCBufferFormattedSeedDensity(v).select($"FeatureKeyValueHashed"))
+        .reduce(_ union _)
+
+      // for example: delete keys in day 1 that do not exist in day 2 to day 6
+      val keysToDelete = featureKeysToScan
+        .except(featureKeysToCompareWith)
+        .as[String]
+        .cache()
+
+      val keysToDeleteCount = keysToDelete.count()
+      val keysToScanCount = featureKeysToScan.count()
+
+      // if too many keys will be deleted, that probably means today's data is corrupted
+      if (keysToDeleteCount * 1.0 / keysToScanCount > maxDeletionPercentage) {
+        throw new Exception(s"Too many keys to delete, keys to scan count: $keysToScanCount keys to delete count: $keysToDeleteCount")
+      }
+
+     keysToDelete
+    }
+    catch {
+      case e: Exception if e.getMessage.contains("Too many keys to delete") =>
+        throw e
+      case e: Exception =>
+        Logger.log(s"current file does not exist, Exception: ${e.toString}")
+        null
+    }
+  }
+
+  private def writeNewRecordsToAerospike(
+                                          records: Dataset[SeedDensityFeature],
+                                          featureVersion: String,
+                                          aerospikeSeedHost: String,
+                                          aerospikePort: Int,
+                                          ttl: Int,
+                                          aerospikeMaxConcurrencyPerTask: Int,
+                                          numEventLoops: Int,
+                                          aerospikeSecrets: AerospikeSecrets,
+                                          aerospikeNamespace: String,
+                                          aerospikeSet: String
+                                        ): (Long, Long) = {
+    records
       .map(row => {
         AerospikeSeedDensityScoreRecord(
           row.FeatureKeyValueHashed,
-          ttl,
           row.data,
           featureVersion.toLong
         )
@@ -111,7 +233,6 @@ object DailySeedMappingIdDensityScoreAerospikePublishingJob extends DensityFeatu
             val key = new Key(aerospikeNamespace, aerospikeSet, record.id)
             val bin_d = new Bin("d", record.d)
             val bin_v = new Bin("v", record.v)
-            val bin_tl = new Bin("tl", record.tl)
 
             val eventLoop = aerospikeClientFactory.eventLoops.next()
             val eventLoopIndex = eventLoop.getIndex
@@ -123,7 +244,7 @@ object DailySeedMappingIdDensityScoreAerospikePublishingJob extends DensityFeatu
                   new AsyncWriteListener(aerospikeClientFactory.throttles, eventLoopIndex, atomicCounter),
                   aerospikeClientFactory.writePolicy,
                   key,
-                  bin_d, bin_v, bin_tl
+                  bin_d, bin_v
                 )
               } catch {
                 case e: Exception =>
@@ -154,12 +275,86 @@ object DailySeedMappingIdDensityScoreAerospikePublishingJob extends DensityFeatu
       )
       .as[(Long, Long)]
       .head
+  }
 
-    FSUtils.writeStringToFile(successFilePath, s"successCount:${writeResults._1} failCount:${writeResults._2}")
+  private def deleteOldRecordsFromAerospike(
+                                             records: Dataset[String],
+                                             aerospikeSeedHost: String,
+                                             aerospikePort: Int,
+                                             ttl: Int,
+                                             aerospikeMaxConcurrencyPerTask: Int,
+                                             numEventLoops: Int,
+                                             aerospikeSecrets: AerospikeSecrets,
+                                             aerospikeNamespace: String,
+                                             aerospikeSet: String
+                                           ): (Long, Long, Long) = {
+    records
+      .mapPartitions(p => {
+        AerospikeCredsAndSecretsUtils.install()
+        p
+      })
+      .rdd
+      .mapPartitions(
+        p => {
+          val aerospikeClientFactory = AerospikeClientFactory(
+            aerospikeSeedHost,
+            aerospikePort,
+            ttl,
+            aerospikeMaxConcurrencyPerTask,
+            numEventLoops,
+            aerospikeSecrets
+          )
+          val aerospikeClient = aerospikeClientFactory.initializeAndCreateAerospikeClient()
 
-    metricsAerospikeWriteGauge.labels("success").set(writeResults._1)
-    metricsAerospikeWriteGauge.labels("fail").set(writeResults._2)
-    prometheus.pushMetrics()
+          var numRecords = 0;
+          val atomicCounter = AerospikeDeleteCounters()
+
+          p.foreach(record => {
+            numRecords += 1
+            val key = new Key(aerospikeNamespace, aerospikeSet, record)
+
+            val eventLoop = aerospikeClientFactory.eventLoops.next()
+            val eventLoopIndex = eventLoop.getIndex
+
+            if (aerospikeClientFactory.throttles.waitForSlot(eventLoopIndex, 1)) {
+              try {
+                aerospikeClient.delete(
+                  eventLoop,
+                  new AsyncDeleteListener(aerospikeClientFactory.throttles, eventLoopIndex, atomicCounter),
+                  aerospikeClientFactory.writePolicy,
+                  key,
+                )
+              } catch {
+                case e: Exception =>
+                  Logger.log("delete exception " + e.toString)
+                  atomicCounter.failCounter.incrementAndGet()
+                  aerospikeClientFactory.throttles.addSlot(eventLoopIndex, 1)
+              }
+            } else {
+              Logger.log("failed to get slot")
+              atomicCounter.failCounter.incrementAndGet()
+            }
+          })
+
+          while (atomicCounter.totalCount() != numRecords) {
+            Logger.log("waiting for all records' deletion to complete")
+            Thread.sleep(1000)
+          }
+
+          aerospikeClient.close()
+          aerospikeClientFactory.close()
+
+          Seq((atomicCounter.successCounter.get(), atomicCounter.failCounter.get(), atomicCounter.deletedCounter.get())).toIterator
+        }
+      )
+      .toDF("SuccessCount", "FailCount", "DeletedCount")
+      .agg(
+        sum($"SuccessCount".as("SuccessCount")),
+        sum($"FailCount".as("FailCount")),
+        sum($"DeletedCount".as("DeletedCount"))
+      )
+      .as[(Long, Long, Long)]
+      .head
   }
 
   private def detectIfServerIsListening(host: String, port: Int): Boolean = {
@@ -181,7 +376,6 @@ object DailySeedMappingIdDensityScoreAerospikePublishingJob extends DensityFeatu
 // record in aerospike
 case class AerospikeSeedDensityScoreRecord(
                                             id: String,
-                                            tl: Int, // ttl
                                             d: Array[Byte],
                                             v: Long
                                           )
@@ -253,6 +447,33 @@ class AsyncWriteListener(throttles: Throttles, eventLoopIndex: Int, counter: Aer
     counter.failCounter.incrementAndGet()
     throttles.addSlot(eventLoopIndex, 1)
     Logger.log(s"write failed with: ${ae.toString}")
+  }
+}
+
+case class AerospikeDeleteCounters(
+                                    successCounter: AtomicLong = new AtomicLong(0L),
+                                    failCounter: AtomicLong = new AtomicLong(0L),
+                                    deletedCounter: AtomicLong = new AtomicLong(0L)
+                                  ) {
+  def totalCount(): Long = {
+    successCounter.get() + failCounter.get()
+  }
+}
+
+class AsyncDeleteListener(throttles: Throttles, eventLoopIndex: Int, counter: AerospikeDeleteCounters) extends DeleteListener {
+
+  override def onSuccess(key: Key, existed: Boolean): Unit = {
+    counter.successCounter.incrementAndGet()
+    if (existed) {
+      counter.deletedCounter.incrementAndGet()
+    }
+    throttles.addSlot(eventLoopIndex, 1)
+  }
+
+  override def onFailure(ae: AerospikeException): Unit = {
+    counter.deletedCounter.incrementAndGet()
+    throttles.addSlot(eventLoopIndex, 1)
+    Logger.log(s"Delete failed with: ${ae.toString}")
   }
 }
 
