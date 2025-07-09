18dab7a3e125d168944694360ffdbc756f8b2145
Red Eshetu <red.eshetu@thetradedesk.com>
2025-07-09 22:58:27 +0000

    rae-DATPERF-6601-no-id-coalesce touch

--- audience/src/main/scala/com/thetradedesk/audience/jobs/dealscore/AvailsFindDealScores.scala
+++ audience/src/main/scala/com/thetradedesk/audience/jobs/dealscore/AvailsFindDealScores.scala
@@ -1,6 +1,6 @@
 package com.thetradedesk.audience.jobs.dealscore
 
-import com.thetradedesk.audience.jobs.modelinput.rsmv2.usersampling.SIBSampler.isDeviceIdSampled1Percent
+import com.thetradedesk.audience.jobs.modelinput.rsmv2.usersampling.SIBSampler.{_isDeviceIdSampled1Percent, _isDeviceIdSampledNPercent}
 import com.thetradedesk.availspipeline.spark.datasets.{DealAvailsAggDailyDataSet, IdentityAndDealAggHourlyDataSet}
 import com.thetradedesk.audience.{date, dateFormatter, ttdEnv}
 import com.thetradedesk.spark.TTDSparkContext.spark
@@ -15,23 +15,16 @@ import java.time.format.DateTimeFormatter
 object AvailsFindDealScores {
   val dateStr = date.format(dateFormatter)
   val dateStrDash = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-  val br_emb_path= config.getString(
-    "br_emb_path", s"s3://thetradedesk-mlplatform-us-east-1/data/${ttdEnv}/audience/RSMV2/deal_score/emb/raw/v=1/date=${dateStr}/")
   val deal_sv_path = config.getString("deal_sv_path", s"s3://ttd-deal-quality/env=prod/VerticaExportDealRelevanceSeedMapping/VerticaAws/date=${dateStr}/")
-  //val avails_path = config.getString(
-  //  "avails_path", s"s3://thetradedesk-useast-avails/datasets/withPII/prod/identity-deal-agg-hourly-delta/date=${dateStrDash}")
-  val users_scores_path = config.getString("users_scores_path", s"s3://thetradedesk-mlplatform-us-east-1/data/${ttdEnv}/audience/scores/tdid2seedid/v=1/date=${dateStr}/")
-  val seed_id_path = config.getString(
-    "seed_id_path", s"s3://thetradedesk-mlplatform-us-east-1/data/${ttdEnv}/audience/scores/seedids/v=2/date=${dateStr}/")
-
-  val out_path = config.getString("out_path", s"s3://thetradedesk-mlplatform-us-east-1/data/${ttdEnv}/audience/deal_score/v=1/date=${dateStr}/")
-  val agg_score_out_path = config.getString("agg_score_out_path", s"s3://thetradedesk-mlplatform-us-east-1/data/${ttdEnv}/audience/deal_score_agg/v=1/date=${dateStr}/")
+  val out_path = config.getString("out_path", s"s3://thetradedesk-mlplatform-us-east-1/data/${ttdEnv}/audience/deal_score_tdids/v=1/date=${dateStr}/")
 
   val num_workers = config.getInt("num_workers", 200 )
   val cpu_per_worker = config.getInt("cpu_per_worker", 32 )
 
-  val cap_users = config.getInt("cap_users", 15000)
-  val min_users = config.getInt("min_users", 3000)
+  val samplingRate = config.getInt("sampling_rate", 3)
+  val min_hour = config.getInt("min_hour", 0)
+  val max_hour = config.getInt("max_hour", 24)
+  val limit_users = config.getInt("limit_users", 50000)
 
   case class UserIdentifier(identitySource: Int, guidString: String)
 
@@ -50,14 +43,13 @@ object AvailsFindDealScores {
   //        CrossDevice = 8, // !!Important!! This value is deliberately not in dbo.IdentitySource in the database.
   //        // This is a fake value used when updating the adfrequency source used (see the function GetLoggedBitmapFromIdentitySources for example).
   //        DATId = 9,
-  private def processUserIdentifiers(userIdentifiers: Seq[UserIdentifier]): String = {
+  private def processUserIdentifiers(userIdentifiers: Seq[UserIdentifier]): Seq[String] = {
     val priorityOrder = Seq(1, 2, 3, 4, 5)
     userIdentifiers
       .filter(ui => priorityOrder.contains(ui.identitySource))
-      .sortBy(ui => priorityOrder.indexOf(ui.identitySource))
-      .headOption
+      .filter(ui => _isDeviceIdSampledNPercent(ui.guidString, samplingRate))
+      .filter(ui => ui.guidString.length > 8 && ui.guidString.charAt(8) == '-')
       .map(_.guidString)
-      .getOrElse("")
   }
   private val processUserIdentifiersUDF: UserDefinedFunction = udf(processUserIdentifiers _)
 
@@ -68,85 +60,43 @@ object AvailsFindDealScores {
     val dealSvDS = dealSvSeedDS.select("DealCode", "SupplyVendorId").distinct()
 
     val dayToRead = LocalDate.parse(dateStrDash).atStartOfDay()
-    val avails = IdentityAndDealAggHourlyDataSet.readFullDayPartition(dayToRead)
+    val avails = IdentityAndDealAggHourlyDataSet.readFullDayPartition(dayToRead).filter(col("hour") >= min_hour && col("hour") < max_hour)
 
     val availsFiltered = avails.filter(
         size(col("DealCodes")) > 0 && !col("DealCodes")(0).isNull && col("DealCodes")(0) =!= "" &&
         expr("exists(UserIdentifiers, x -> x.identitySource IN (1,2,3,4,5))")
     ).select("UserIdentifiers", "DealCodes", "SupplyVendorId")
 
-    val availsProcessed = availsFiltered.withColumn("TDID", processUserIdentifiersUDF(col("UserIdentifiers"))).select("TDID", "DealCodes", "SupplyVendorId")
+    val availsProcessed = availsFiltered.withColumn("Identifiers", processUserIdentifiersUDF(col("UserIdentifiers")))
+      .filter(size(col("Identifiers")) > 0)
+      .select("Identifiers", "DealCodes", "SupplyVendorId")
 
-    val availsSampled = availsProcessed
-      .filter(isDeviceIdSampled1Percent('TDID))
-      .filter("SUBSTRING(TDID, 9, 1) = '-'") // Assure TDID is in uuid format, same logic is applied in offline user scores job
-      .select('TDID,
-        'DealCodes,
-        'SupplyVendorId)
+    val availsProcessedExploded = availsProcessed.withColumn("TDID", explode(col("Identifiers"))).withColumn("DealCode", explode(col("DealCodes"))).select('TDID, 'DealCode, 'SupplyVendorId)
 
-    val explodedAvailsProcessed = availsSampled.withColumn("DealCode", explode(col("DealCodes"))).select('TDID, 'DealCode, 'SupplyVendorId)
-
-    //TODO verify the need for trim
-    val matched_df = explodedAvailsProcessed.as("a").join(
+    val matched_df = availsProcessedExploded.as("a").join(
       broadcast(dealSvDS.as("d")),
       trim(col("a.DealCode")) === trim(col("d.DealCode")) && col("a.SupplyVendorId") === col("d.SupplyVendorId")
-    )
+    ).select("TDID", "a.DealCode", "a.SupplyVendorId")
 
-    //TODO might need to count by TDID to do a weighted average
-    val TdidsByDCSV =  matched_df.select("TDID", "a.DealCode", "a.SupplyVendorId").distinct()
+    val TdidsByDCSV = matched_df.select("TDID", "DealCode", "SupplyVendorId").distinct()
 
-    val user_scores = spark.read.format("parquet").load(users_scores_path)
-    val seedIds = spark.read.format("parquet").load(seed_id_path)
-
-    val posexplodedSeedsWithRowNumber = seedIds.selectExpr("posexplode(SeedId) as (pos, SeedId)")
-
-    //formula is 4 * cpu * number of machines
     val numberOfPartitions = num_workers * 4 * cpu_per_worker
-    val TdidsWithScore = TdidsByDCSV.repartition(numberOfPartitions, col("TDID")).join(user_scores.repartition(numberOfPartitions, col("TDID")), Seq("TDID")).select(TdidsByDCSV("TDID"), TdidsByDCSV("DealCode"), TdidsByDCSV("SupplyVendorId"), user_scores("Score"))
-    val TdisWithScoreWithSdf = TdidsWithScore.join(broadcast(dealSvSeedDS), trim(TdidsWithScore("DealCode")) === trim(dealSvSeedDS("DealCode")) && TdidsWithScore("SupplyVendorId") === dealSvSeedDS("SupplyVendorId"), "inner").select(col("TDID"), TdidsWithScore("DealCode"), TdidsWithScore("SupplyVendorId"), col("Score"), col("SeedId"))
+    val windowSpec = Window.partitionBy("DealCode", "SupplyVendorId").orderBy(col("rand"))
 
-    val finalDf = TdisWithScoreWithSdf.join(broadcast(posexplodedSeedsWithRowNumber), Seq("SeedId")).withColumn("Score", element_at(col("Score"), col("pos") + 1)).select(TdisWithScoreWithSdf("TDID"), TdisWithScoreWithSdf("DealCode"), TdisWithScoreWithSdf("SupplyVendorId"),  TdisWithScoreWithSdf("SeedId"), col("Score"))
-
-    val structuredFinal = finalDf.withColumn("tdidValueStruct", struct(col("TDID"), col("Score")))
-
-    val windowSpec = Window.partitionBy("DealCode", "SupplyVendorId", "SeedId").orderBy(col("rand"))
-    val structuredFinalWithRowNum = structuredFinal
-      .repartition(numberOfPartitions, col("DealCode"), col("SupplyVendorId"), col("SeedId"))
+    TdidsByDCSV
+      .repartition(numberOfPartitions, col("DealCode"), col("SupplyVendorId"))
       .withColumn("rand", rand())
       .withColumn("row_num", row_number().over(windowSpec))
-      .filter(col("row_num") <= cap_users)
+      .filter(col("row_num") <= limit_users)
       .drop("row_num")
-
-    val raw_out = structuredFinalWithRowNum
-      .groupBy("DealCode", "SupplyVendorId", "SeedId")
-      .agg(collect_list(col("tdidValueStruct")).as("Scores"))
-      //.withColumn("Scores", expr(s"slice(Scores, 1, $cap_users)"))
-
-    raw_out
       .write
       .format("parquet")
       .mode("overwrite")
       .option("codec", "org.apache.hadoop.io.compress.GzipCodec")
       .save(out_path)
-
-    val raw_scores = spark.read.format("parquet").load(out_path)
-    raw_scores.withColumn("Score", explode($"Scores.Score"))
-      .groupBy("DealCode", "SupplyVendorId", "SeedId")
-      .agg(
-        count("Score").as("UserCount"),
-        avg("Score").as("AverageScore")
-      )
-      .filter(col("UserCount") >= min_users)
-      .write
-      .format("parquet")
-      .mode("overwrite")
-      .option("codec", "org.apache.hadoop.io.compress.GzipCodec")
-      .save(agg_score_out_path)
-
   }
 
   def main(args: Array[String]): Unit = {
     runETLPipeline()
   }
 }
-
