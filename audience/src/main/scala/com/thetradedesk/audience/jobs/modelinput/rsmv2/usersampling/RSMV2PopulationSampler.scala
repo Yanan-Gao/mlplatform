5a8bbb871246fd7c2188bdecebff91a2a2128dce
YananGao <36028431+Yanan-Gao@users.noreply.github.com>
2025-08-20 21:07:16 +0800

    Avoid reflection result type mismatch

--- audience/src/main/scala/com/thetradedesk/audience/jobs/modelinput/rsmv2/usersampling/RSMV2PopulationSampler.scala
+++ audience/src/main/scala/com/thetradedesk/audience/jobs/modelinput/rsmv2/usersampling/RSMV2PopulationSampler.scala
@@ -1,18 +1,33 @@
 package com.thetradedesk.audience.jobs.modelinput.rsmv2.usersampling
 
-import com.thetradedesk.audience.jobs.modelinput.rsmv2.RelevanceModelInputGeneratorJobConfig
 import com.thetradedesk.audience.shouldTrackTDID
 import com.thetradedesk.spark.TTDSparkContext.spark.implicits._
 import org.apache.spark.sql.Column
 import org.apache.spark.sql.functions._
+import SamplerConfigUtils._
 
-case class RSMV2PopulationSampler(conf: RelevanceModelInputGeneratorJobConfig) extends Sampler {
+/** Sampler used for generating population statistics. Configuration requires
+  * a `sampleSalt` and a sequence of `sampleIndexes` indicating which hashed
+  * buckets should be kept.
+  */
+case class RSMV2PopulationSampler(conf: Any) extends Sampler {
+
+  private val sampleSalt: String =
+    getString(conf, "sampleSalt")
+      .orElse(getString(conf, "RSMV2UserSampleSalt"))
+      .getOrElse(throw new IllegalArgumentException("sampleSalt is required"))
+
+  private val sampleIndexes: Seq[Int] =
+    getSeqInt(conf, "sampleIndexes")
+      .orElse(getSeqInt(conf, "RSMV2PopulationUserSampleIndex"))
+      .getOrElse(throw new IllegalArgumentException("sampleIndexes are required"))
 
   override def samplingFunction(symbol: Symbol): Column = {
-    val hashedValue = abs(xxhash64(concat(symbol, lit(conf.RSMV2UserSampleSalt)))) % lit(10)
+    val hashedValue = abs(xxhash64(concat(symbol, lit(sampleSalt)))) % lit(10)
 
     shouldTrackTDID(symbol) &&
       substring(symbol, 9, 1) === lit("-") &&
-      (hashedValue.isin(conf.RSMV2PopulationUserSampleIndex: _*))
+      (hashedValue.isin(sampleIndexes: _*))
   }
 }
+
