eac3c77a114ba85dba2b217b976d094632811a8f
YananGao <36028431+Yanan-Gao@users.noreply.github.com>
2025-08-20 20:45:39 +0800

    Add config extraction utilities (#50)

--- audience/src/main/scala/com/thetradedesk/audience/jobs/modelinput/rsmv2/usersampling/SamplerFactory.scala
+++ audience/src/main/scala/com/thetradedesk/audience/jobs/modelinput/rsmv2/usersampling/SamplerFactory.scala
@@ -1,15 +1,28 @@
 package com.thetradedesk.audience.jobs.modelinput.rsmv2.usersampling
 
-import com.thetradedesk.audience.jobs.modelinput.rsmv2.RelevanceModelInputGeneratorJobConfig
-
+/** Factory for building samplers based on name. The configuration type is kept
+  * generic so that individual samplers can decide how to interpret the values
+  * they need.
+  */
 object SamplerFactory {
-  def fromString(name: String, conf: RelevanceModelInputGeneratorJobConfig): Sampler = {
+
+  /**
+    * Create a [[Sampler]] by name using an arbitrary configuration object.
+    *
+    * @param name identifier of the sampler to build
+    * @param conf configuration object understood by the selected sampler
+    * @return the concrete [[Sampler]] implementation
+    */
+  def fromString(name: String, conf: Any): Sampler = {
     name match {
       case "SIB" => SIBSampler
       case "RSMV2" => RSMV2Sampler(conf)
-      case "RSMV2Measurement" => RSMV2MeasurementSampler(conf) // will help measurement job use split=1 in feature_store to speed up the process
-      case "RSMV2Population" => RSMV2PopulationSampler(conf) // will help measurement job use split=1 in feature_store to speed up the process
+      case "RSMV2Measurement" =>
+        RSMV2MeasurementSampler(conf) // used by measurement job to speed up process
+      case "RSMV2Population" =>
+        RSMV2PopulationSampler(conf) // used by measurement job to speed up process
       case _ => throw new IllegalArgumentException(s"Unknown sampler: $name")
     }
   }
 }
+
