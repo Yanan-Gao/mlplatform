c81c600e6af6076fbc01002a9a19bc4e6610ff9c
Freeman Chen <freeman.chen@thetradedesk.com>
2025-09-22 02:07:34 +0000

    frc-AUDAUTO-3903-add-feature-transformation add more features in ETL

--- audience/src/main/scala/com/thetradedesk/audience/jobs/modelinput/relevanceboostmodel/BoostModelInputGeneratorJob.scala
+++ audience/src/main/scala/com/thetradedesk/audience/jobs/modelinput/relevanceboostmodel/BoostModelInputGeneratorJob.scala
@@ -1,7 +1,7 @@
 package com.thetradedesk.audience.jobs.modelinput.relevanceboostmodel
-
+import com.thetradedesk.audience.jobs.modelinput.rsmv2.RSMV2SharedFunction.{SubFolder}
 import com.thetradedesk.audience.configs.AudienceModelInputGeneratorConfig
-import com.thetradedesk.audience.datasets.{AudienceModelPolicyReadableDataset, RelevanceBoostModelInputDataset, RelevanceBoostModelInputRecord}
+import com.thetradedesk.audience.datasets.{AudienceModelPolicyReadableDataset, RelevanceBoostModelInputDataset, RelevanceBoostModelInputRecord, SegmentDataSource}
 import com.thetradedesk.audience._
 import org.apache.spark.sql._
 import com.thetradedesk.spark.TTDSparkContext.spark
@@ -12,16 +12,164 @@ import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 import org.apache.spark.sql.expressions.Aggregator
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
-
+import org.apache.spark.sql.{DataFrame, Column}
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import scala.collection.mutable
+import com.tdunning.math.stats.MergingDigest
+import java.nio.ByteBuffer
+import com.thetradedesk.featurestore.transform.{QuantileSummaryAgg, QuantileSummaryMergeAgg}
 
 object RelevanceBoostModelInputGeneratorJob {
+  object Config {
+    val dataSources = config.getString("dataSources", "Fpd,Tpd,ExtendedFpd").split(",").map(dataSource => SegmentDataSource.withName(dataSource))
+    val groupCols = config.getString("groupCols", "BidRequestId,SeedId").split(",")
+    val conditionCol = config.getString("conditionCol", "DataSource")
+    val prefix = config.getString("prefix", "Matched")
+    val valueCol = config.getString("valueCol", "RelevanceRatio")
+    val idCol = config.getString("idCol", "TargetingDataId")
+    val topN = config.getInt("topN", 5)
+    val percentiles = config.getString("percentiles", "50,90").split(",").map(percentile => percentile.toInt)
+    val log_buffer = config.getDouble("log_buffer", 1.0).toFloat
+    
+  }
 
   def getDateStr(date: LocalDate): String = {
     val dtf = DateTimeFormatter.ofPattern("yyyyMMdd")
     date.format(dtf)
+  }
+
+  val mergeTDigest = udf { (a: Array[Byte], b: Array[Byte]) =>
+      (Option(a), Option(b)) match {
+        case (None, None)    => null
+        case (Some(x), None) => x
+        case (None, Some(y)) => y
+        case (Some(x), Some(y)) =>
+          val da = MergingDigest.fromBytes(ByteBuffer.wrap(x))
+          val db = MergingDigest.fromBytes(ByteBuffer.wrap(y))
+          da.add(db) 
+          val res = ByteBuffer.allocate(da.smallByteSize())
+          da.asSmallBytes(res)   
+          res.array()
+      }
+    }
+  
+  val getPercentiles = udf(
+      (bytes: Array[Byte], percentiles: Seq[Float]) =>{
+        if (bytes == null || percentiles == null) null
+        else {
+          val digest = MergingDigest.fromBytes(ByteBuffer.wrap(bytes))
+          percentiles.map(p => p -> digest.quantile(p.toFloat/100.0)).toMap
+        }
+      }
+    )
+
+
+  def featureTransform(df: DataFrame, 
+                      dataSources: Seq[SegmentDataSource.Value], 
+                      groupCols: Seq[String] = Seq("BidRequestId","SeedId"),
+                      conditionCol:String = "DataSource", 
+                      prefix: String = "Matched", 
+                      valueCol: String = "RelevanceRatio",
+                      idCol: String = "TargetingDataId",
+                      topN: Int = 5,
+                      percentiles: Seq[Int] = Seq(50,90),
+                      log_buffer: Float = 1.0f
+                      ) = {
+    
+    val mergeTargetingDataRelevanceScores = udaf(TargetingDataRelevanceScoreAggregator)
+    val mergePercentileScore = udaf(new QuantileSummaryAgg())
+
+    val aggCols: Seq[Column] = dataSources.flatMap { ds =>
+      
+      val condition = col(conditionCol) === lit(ds.id)              
+      val rr = when(condition, col(valueCol))
+      val td = when(condition, col(idCol))
+
+      val source = ds.toString    
+
+      val relevanceValues = filter(collect_list(rr), x => x.isNotNull && !isnan(x))
+      val sortedRelevance = sort_array(relevanceValues, asc = false)
+
+      val topCols: Seq[Column] =
+        (1 to topN).map { k =>
+          coalesce(element_at(sortedRelevance, k), lit(0.0)).cast(FloatType).as(s"${prefix}${source}RelevanceTop${k}")
+        }
+                             
+      Seq(
+        mergeTargetingDataRelevanceScores(td, rr).as(s"${source}RelevanceMap"),
+        mergePercentileScore(rr).as(s"${prefix}${source}RelevanceArrayBytes"),
+        coalesce(sum(rr),lit(0)).cast(FloatType)
+          .as(s"${prefix}${source}RelevanceSum"),
+        coalesce(stddev(rr),lit(0)).cast(FloatType)
+          .as(s"${prefix}${source}RelevanceStddev"),
+        coalesce(count(when(condition, col(valueCol))), lit(0)).cast(FloatType)
+          .as(s"${prefix}${source}Count")
+        ) ++ topCols
+      }
+
+    val agg_df = df.groupBy(groupCols.map(col): _*).agg(aggCols.head, aggCols.tail: _*)
+
+    val extendTopNCols: Seq[Column] = (1 to topN).map { k =>
+      val data_sources = Seq(SegmentDataSource.Fpd.toString, SegmentDataSource.ExtendedFpd.toString)
+      val colsUpToK = data_sources.flatMap(ds => (1 to k).map(i => col(s"${prefix}${ds}RelevanceTop${i}")))
+      val kthLargest = element_at(sort_array(filter(array(colsUpToK: _*), x => x.isNotNull)), -k)
+      kthLargest.cast(FloatType).alias(s"${prefix}${SegmentDataSource.ExtendedFpd}TopRelevance${k}")
+    }
+
+    val pctArrayCols: Seq[Column] = dataSources.map {
+      ds => getPercentiles(col(s"${prefix}${ds.toString}RelevanceArrayBytes"), typedLit(percentiles))
+          .alias(s"${prefix}${ds.toString}RelevancePercentiles")
+    }
+
+    def extractPercentilesCol(mapCol: Column, dataSource: String, percentiles: Seq[Int]): Seq[Column] =
+      percentiles.map(p => element_at(mapCol, lit(p)).cast(FloatType) .as(s"${prefix}${dataSource}RelevanceP${p}"))
+
+    val percentilesCols: Seq[Column] = dataSources.flatMap(ds => extractPercentilesCol(col(s"${prefix}${ds.toString}RelevancePercentiles"), ds.toString, percentiles))
+    
+
+    def combinedStddev(n1: Column, s1: Column, sum1: Column, n2: Column, s2: Column, sum2: Column): Column = {
+
+      val N = n1 + n2
+
+      val mu1 = when(n1 > 0.0, sum1 / n1).otherwise(lit(null).cast(FloatType))
+      val mu2 = when(n2 > 0.0, sum2 / n2).otherwise(lit(null).cast(FloatType))
+
+      val ssWithin =
+        when(n1 > 1.0, (n1 - 1.0) * pow(s1, 2)).otherwise(lit(0.0)) +
+        when(n2 > 1.0, (n2 - 1.0) * pow(s2, 2)).otherwise(lit(0.0))
+
+      val ssBetween = when((n1 > 0.0) && (n2 > 0.0),
+        (n1 * n2 / N) * pow(mu1 - mu2, 2)
+      ).otherwise(lit(0.0))
+
+      val varUnion = when(N > 1.0, (ssWithin + ssBetween) / (N - 1.0))
+        .otherwise(lit(null).cast(FloatType))
+
+      sqrt(varUnion)
+    }
+
+    val res = agg_df
+                .withColumn( s"${prefix}${SegmentDataSource.ExtendedFpd}RelevanceArrayBytes", mergeTDigest(col(s"${prefix}${SegmentDataSource.Fpd}RelevanceArrayBytes"),col(s"${prefix}${SegmentDataSource.ExtendedFpd}RelevanceArrayBytes")))
+                .withColumn(s"${prefix}${SegmentDataSource.ExtendedFpd}RelevanceStddev", combinedStddev(col(s"${prefix}${SegmentDataSource.ExtendedFpd}Count"), 
+                                                                                                        col(s"${prefix}${SegmentDataSource.ExtendedFpd}RelevanceStddev"), 
+                                                                                                        col(s"${prefix}${SegmentDataSource.ExtendedFpd}RelevanceSum"),
+                                                                                                        col(s"${prefix}${SegmentDataSource.Fpd}Count"), 
+                                                                                                        col(s"${prefix}${SegmentDataSource.Fpd}RelevanceStddev"), 
+                                                                                                        col(s"${prefix}${SegmentDataSource.Fpd}RelevanceSum")))
+                .withColumn(s"${prefix}${SegmentDataSource.ExtendedFpd}Count", col(s"${prefix}${SegmentDataSource.ExtendedFpd}Count") + col(s"${prefix}${SegmentDataSource.Fpd}Count"))
+                .withColumn(s"${prefix}${SegmentDataSource.ExtendedFpd}RelevanceSum", col(s"${prefix}${SegmentDataSource.ExtendedFpd}RelevanceSum") + col(s"${prefix}${SegmentDataSource.Fpd}RelevanceSum"))
+                
+                
+                
+                .select((pctArrayCols ++ agg_df.columns.map(col)):_*)
+                .select((percentilesCols ++ extendTopNCols ++ agg_df.columns.map(col)):_*)
+
+    val resCols = res.columns.map(col) ++ res.schema.collect {
+            case f if f.name.startsWith(prefix) && f.dataType.isInstanceOf[NumericType] => log(col(f.name) + lit(log_buffer)).cast(FloatType).as(s"f_${f.name}")
+          }
+    
+    res.select(resCols: _*)
   }
 
   def transform(
@@ -29,7 +177,7 @@ object RelevanceBoostModelInputGeneratorJob {
                  oosPredictionDf: DataFrame,
                  geronimoDf: DataFrame,
                  lalResultsDf: DataFrame
-               ): Dataset[RelevanceBoostModelInputRecord] = {
+               ): DataFrame = {
 
     val policyTable = policyTableDf
       .where('CrossDeviceVendorId === 0 && 'Source === 3)
@@ -48,9 +196,14 @@ object RelevanceBoostModelInputGeneratorJob {
       .filter($"Original_NeoScore".isNotNull)
       .select($"BidRequestId", $"Target", $"IsSensitive", $"Original_NeoScore", $"SourceId".as("SeedId"), $"ZipSiteLevel_Seed")
 
+
     val geronimo = geronimoDf
       .filter($"IsImp"===1)
       .select($"BidRequestId", $"AdvertiserId", $"CampaignId", $"AdGroupId", $"UserTargetingDataIds".as("TargetingDataIds"), $"AdvertiserFirstPartyDataIds")
+      .withColumn("AdvertiserFirstPartyDataIds", 
+                    when(col("AdvertiserFirstPartyDataIds").isNull, col("AdvertiserFirstPartyDataIds")) 
+                    .otherwise(expr("array_except(AdvertiserFirstPartyDataIds, coalesce(TargetingDataIds, array()))"))
+                    )
 
     val oos = geronimo
       .join(oosPrediction, Seq("BidRequestId"), "inner")
@@ -62,48 +215,48 @@ object RelevanceBoostModelInputGeneratorJob {
       .select($"BidRequestId", $"SeedId", explode($"AdvertiserFirstPartyDataIds").as("TargetingDataId"))
 
     val lalResults = lalResultsDf
-      .select($"TargetingDataId", $"RelevanceRatio", $"SeedId", $"IsFirstPartyData")
+      .select($"TargetingDataId", $"RelevanceRatio", $"SeedId", $"DataSource")
+      .cache() 
 
-    val mergeTargetingDataRelevanceScores = udaf(TargetingDataRelevanceScoreAggregator)
-
-    val oosAdvertiserFirstPartyDataMap = oosAdvertiserFirstPartyDataExploded
+    val oosAdvertiserFirstPartyDataExplodedWithSource = oosAdvertiserFirstPartyDataExploded
+                                      .join(lalResults, Seq("SeedId", "TargetingDataId"), "left")
+                                      .withColumn("DataSource", lit(SegmentDataSource.ExtendedFpd.id))
+    
+    val oosTargetingDataExplodedWithSource = oosTargetingDataExploded
       .join(lalResults, Seq("SeedId", "TargetingDataId"), "left")
       .withColumn("RelevanceRatio", when($"RelevanceRatio".isNotNull, $"RelevanceRatio").otherwise(lit(0.0)))
-      .groupBy("BidRequestId", "SeedId")
-      .agg(
-        mergeTargetingDataRelevanceScores($"TargetingDataId", $"RelevanceRatio").as("AdvertiserFirstPartyDataRelevanceMap")
-      )
 
-    oosTargetingDataExploded
-      .join(lalResults, Seq("SeedId", "TargetingDataId"), "left")
-      .withColumn("RelevanceRatio", when($"RelevanceRatio".isNotNull, $"RelevanceRatio").otherwise(lit(0.0)))
-      .groupBy("BidRequestId", "SeedId")
-      // aggregate statistics
-      .agg(
-        mergeTargetingDataRelevanceScores($"TargetingDataId", $"RelevanceRatio").as("DataRelevanceMap"),
+    val oosTargetingDataExplodedFull = oosAdvertiserFirstPartyDataExplodedWithSource
+                                        .unionByName(oosTargetingDataExplodedWithSource)
+                                        .withColumn("RelevanceRatio", when($"RelevanceRatio".isNotNull, $"RelevanceRatio").otherwise(lit(0.0)))
 
-        // third party data features
-        percentile_approx(when($"IsFirstPartyData" === false, $"RelevanceRatio"), lit(0.5), lit(10000)).cast(FloatType).as("MatchedTpdRelevanceP50"),
-        percentile_approx(when($"IsFirstPartyData" === false, $"RelevanceRatio"), lit(0.9), lit(10000)).cast(FloatType).as("MatchedTpdRelevanceP90"),
-        sum(when($"IsFirstPartyData" === false, $"RelevanceRatio")).cast(FloatType).as("MatchedTpdRelevanceSum"),
-        stddev(when($"IsFirstPartyData" === false, $"RelevanceRatio")).cast(FloatType).as("MatchedTpdRelevanceStddev"),
-        count(when($"IsFirstPartyData" === false, $"TargetingDataId")).cast(IntegerType).as("MatchedTpdCount"),
 
-        // first party data features
-        percentile_approx(when($"IsFirstPartyData" === true, $"RelevanceRatio"), lit(0.5), lit(10000)).cast(FloatType).as("MatchedFpdRelevanceP50"),
-        percentile_approx(when($"IsFirstPartyData" === true, $"RelevanceRatio"), lit(0.9), lit(10000)).cast(FloatType).as("MatchedFpdRelevanceP90"),
-        sum(when($"IsFirstPartyData" === true, $"RelevanceRatio")).cast(FloatType).as("MatchedFpdRelevanceSum"),
-        stddev(when($"IsFirstPartyData" === true, $"RelevanceRatio")).cast(FloatType).as("MatchedFpdRelevanceStddev"),
-        count(when($"IsFirstPartyData" === true, $"TargetingDataId")).cast(IntegerType).as("MatchedFpdCount")
-      )
-      .join(oosAdvertiserFirstPartyDataMap, Seq("BidRequestId", "SeedId"), "left")
+    val oosWithFeatures = featureTransform(oosTargetingDataExplodedFull, 
+                      Config.dataSources, 
+                      Config.groupCols,
+                      Config.conditionCol, 
+                      Config.prefix, 
+                      Config.valueCol,
+                      Config.idCol,
+                      Config.topN,
+                      Config.percentiles,
+                      Config.log_buffer
+                      )
+
+
+
+
+
+      oosWithFeatures
       .join(oos, Seq("BidRequestId", "SeedId")) // join aggregation result back with oos
-      // multiplication features
-      .withColumn("MatchedFpdRelevanceP90xZipSiteLevel", $"MatchedFpdRelevanceP90" * $"ZipSiteLevel_Seed")
-      .withColumn("MatchedTpdRelevanceP90xZipSiteLevel", $"MatchedTpdRelevanceP90" * $"ZipSiteLevel_Seed")
-      .withColumn("MatchedFpdRelevanceP90xOriginal_NeoScore", $"MatchedFpdRelevanceP90" * $"Original_NeoScore")
-      .withColumn("MatchedTpdRelevanceP90xOriginal_NeoScore", $"MatchedTpdRelevanceP90" * $"Original_NeoScore")
-      .selectAs[RelevanceBoostModelInputRecord]
+       // interaction features
+        .withColumn(s"f_${Config.prefix}${SegmentDataSource.Fpd}RelevanceP90xOriginal_NeoScore", col(s"f_${Config.prefix}${SegmentDataSource.Fpd}RelevanceP90")*col("Original_NeoScore"))
+        .withColumn(s"f_${Config.prefix}${SegmentDataSource.ExtendedFpd}RelevanceP90xOriginal_NeoScore", col(s"f_${Config.prefix}${SegmentDataSource.ExtendedFpd}RelevanceP90")*col("Original_NeoScore"))
+        .withColumn(s"f_${Config.prefix}${SegmentDataSource.Tpd}RelevanceP90xOriginal_NeoScore", col(s"f_${Config.prefix}${SegmentDataSource.Tpd}RelevanceP90")*col("Original_NeoScore"))
+        .withColumn("f_Logit_NeoScore", log(col("Original_NeoScore") / (lit(1.0) - col("Original_NeoScore"))).cast(FloatType))
+        .withColumn("splitReminder", rand())
+        .withColumn("subfolder", when(col("splitReminder")<0.8, SubFolder.Train.id).otherwise(SubFolder.Val.id))
+      
   }
 
   def readLalResults(date: LocalDate): DataFrame = {
@@ -113,10 +266,10 @@ object RelevanceBoostModelInputGeneratorJob {
     val lalDateStr = getDateStr(date.minusDays(2))
 
     val thirdParty = spark.read.parquet(s"s3://ttd-identity/datapipeline/$lalReadEnv/models/rsm_lal/all_model_unfiltered_results/v=1/idCap=100000/date=$lalDateStr")
-      .select($"SeedId", $"TargetingDataId", $"RelevanceRatio", lit(false).as("IsFirstPartyData"))
+      .select($"SeedId", $"TargetingDataId", $"RelevanceRatio", lit(SegmentDataSource.Tpd.id).alias("DataSource"))
 
     val firstParty = spark.read.parquet(s"s3://ttd-identity/datapipeline/$lalReadEnv/models/rsm_lal/1p_all_model_unfiltered_results/v=1/idCap=100000/date=$lalDateStr")
-      .select($"SeedId", $"TargetingDataId", $"RelevanceRatio", lit(true).as("IsFirstPartyData"))
+      .select($"SeedId", $"TargetingDataId", $"RelevanceRatio", lit(SegmentDataSource.Fpd.id).alias("DataSource"))
 
     thirdParty.union(firstParty)
   }
@@ -145,21 +298,40 @@ object RelevanceBoostModelInputGeneratorJob {
 
     val lalDf = readLalResults(date)
 
-    val modelInput = transform(policyTableDf.toDF(), oosPredictionDf, geronimoDf, lalDf)
+    // val audienceTargetingData = AudienceTargetingDataSet().readPartition(date)(spark)
 
-    RelevanceBoostModelInputDataset("Seed_None").writePartition(
-      modelInput,
-      dateTime,
-      saveMode = SaveMode.Overwrite
-    )
+    val modelInput = transform(policyTableDf.toDF(), oosPredictionDf, geronimoDf, lalDf)
+      .cache() 
+
+    try {
+      RelevanceBoostModelInputDataset("Seed_None").writePartition(
+        modelInput.filter('subfolder === lit(SubFolder.Train.id)).as[RelevanceBoostModelInputRecord],
+        dateTime,
+        subFolderKey = Some("split"),
+        subFolderValue = Some(SubFolder.Train.toString),
+        saveMode = SaveMode.Overwrite
+      )
+
+      RelevanceBoostModelInputDataset("Seed_None").writePartition(
+        modelInput.filter('subfolder === lit(SubFolder.Val.id)).as[RelevanceBoostModelInputRecord],
+        dateTime,
+        subFolderKey = Some("split"),
+        subFolderValue = Some(SubFolder.Val.toString),
+        saveMode = SaveMode.Overwrite
+      )
+    } finally {
+      modelInput.unpersist()
+    }
+
   }
 }
 
-object TargetingDataRelevanceScoreAggregator extends Aggregator[(Long, Double), mutable.Map[Long, Double], Map[Long, Double]] with Serializable {
+object TargetingDataRelevanceScoreAggregator extends Aggregator[(java.lang.Long, java.lang.Double), mutable.Map[Long, Double], Map[Long, Double]] with Serializable {
   override def zero: mutable.Map[Long, Double] = mutable.Map.empty
 
-  override def reduce(buffer: mutable.Map[Long, Double], input: (Long, Double)): mutable.Map[Long, Double] = {
-    if (input != null) {
+  override def reduce(buffer: mutable.Map[Long, Double], input: (java.lang.Long, java.lang.Double)): mutable.Map[Long, Double] = {
+
+    if (input._1 != null && input._2 != null) {
       buffer.update(input._1, input._2)
     }
     buffer
--- audience/src/test/scala/com/thetradedesk/audience/jobs/modelinput/relevanceboostmodel/RelevanceBoostModelInputGeneratorTest.scala
+++ audience/src/test/scala/com/thetradedesk/audience/jobs/modelinput/relevanceboostmodel/RelevanceBoostModelInputGeneratorTest.scala
@@ -21,7 +21,7 @@ class RelevanceBoostModelInputGeneratorTest extends TTDSparkTest {
 
     val geronimoDf = Seq(
       ("BidRequestId1", 1, Seq(
-        1000L, 1001L, 1002L, 1003L, 1004L, 1005L, 1006L, 1007L, 1008L, 1009L,
+        1000L, 1001L, 1002L, 1003L, 1004L,
         2000L, 2001L, 2002L, 2003L, 2004L, 2005L, 2006L, 2007L, 2008L, 2009L,
       ), Seq(
         1000L, 1001L, 1002L, 1003L, 1004L, 1005L, 1006L, 1007L, 1008L, 1009L,
@@ -29,28 +29,28 @@ class RelevanceBoostModelInputGeneratorTest extends TTDSparkTest {
     ).toDF("BidRequestId", "IsImp", "UserTargetingDataIds", "AdvertiserFirstPartyDataIds", "AdvertiserId", "CampaignId", "AdGroupId")
 
     val lalResultsDf = Seq(
-      ("SeedId1", 1000L, 0.1, true),
-      ("SeedId1", 1001L, 0.2, true),
-      ("SeedId1", 1002L, 0.3, true),
-      ("SeedId1", 1003L, 0.4, true),
-      ("SeedId1", 1004L, 0.5, true),
-      ("SeedId1", 1005L, 0.6, true),
-      ("SeedId1", 1006L, 0.7, true),
-      ("SeedId1", 1007L, 0.8, true),
-      ("SeedId1", 1008L, 0.9, true),
-      ("SeedId1", 1009L, 1.0, true),
+      ("SeedId1", 1000L, 0.1, 2),
+      ("SeedId1", 1001L, 0.2, 2),
+      ("SeedId1", 1002L, 0.3, 2),
+      ("SeedId1", 1003L, 0.4, 2),
+      ("SeedId1", 1004L, 0.5, 2),
+      ("SeedId1", 1005L, 0.6, 2),
+      ("SeedId1", 1006L, 0.7, 2),
+      ("SeedId1", 1007L, 0.8, 2),
+      ("SeedId1", 1008L, 0.9, 2),
+      ("SeedId1", 1009L, 1.0, 2),
 
-      ("SeedId1", 2000L, 1.1, false),
-      ("SeedId1", 2001L, 1.2, false),
-      ("SeedId1", 2002L, 1.3, false),
-      ("SeedId1", 2003L, 1.4, false),
-      ("SeedId1", 2004L, 1.5, false),
-      ("SeedId1", 2005L, 1.6, false),
-      ("SeedId1", 2006L, 1.7, false),
-      ("SeedId1", 2007L, 1.8, false),
-      ("SeedId1", 2008L, 1.9, false),
-      ("SeedId1", 2009L, 2.0, false),
-    ).toDF("SeedId", "TargetingDataId", "RelevanceRatio", "IsFirstPartyData")
+      ("SeedId1", 2000L, 1.1, 1),
+      ("SeedId1", 2001L, 1.2, 1),
+      ("SeedId1", 2002L, 1.3, 1),
+      ("SeedId1", 2003L, 1.4, 1),
+      ("SeedId1", 2004L, 1.5, 1),
+      ("SeedId1", 2005L, 1.6, 1),
+      ("SeedId1", 2006L, 1.7, 1),
+      ("SeedId1", 2007L, 1.8, 1),
+      ("SeedId1", 2008L, 1.9, 1),
+      ("SeedId1", 2009L, 2.0, 1),
+    ).toDF("SeedId", "TargetingDataId", "RelevanceRatio", "DataSource")
 
     val stringifyMap = udf((m: Map[Long, Double]) => {
       m.toList.sortBy(_._1).mkString(",")
@@ -62,8 +62,9 @@ class RelevanceBoostModelInputGeneratorTest extends TTDSparkTest {
         geronimoDf,
         lalResultsDf
       )
-      .withColumn("DataRelevanceMapStr", stringifyMap($"DataRelevanceMap"))
-      .withColumn("AdvertiserFirstPartyDataRelevanceMapStr", stringifyMap($"AdvertiserFirstPartyDataRelevanceMap"))
+      .withColumn("TpdRelevanceMapStr", stringifyMap($"TpdRelevanceMap"))
+      .withColumn("FpdRelevanceMapStr", stringifyMap($"FpdRelevanceMap"))
+      .withColumn("ExtendedFpdRelevanceMapStr", stringifyMap($"ExtendedFpdRelevanceMap"))
       .select(
         $"BidRequestId",
         $"AdvertiserId",
@@ -74,26 +75,53 @@ class RelevanceBoostModelInputGeneratorTest extends TTDSparkTest {
         $"Original_NeoScore",
         $"SeedId",
         $"ZipSiteLevel_Seed",
-        $"DataRelevanceMapStr",
-        $"AdvertiserFirstPartyDataRelevanceMapStr",
+        $"TpdRelevanceMapStr",
+        $"FpdRelevanceMapStr",
+        $"ExtendedFpdRelevanceMapStr",
+        
+        $"f_MatchedFpdRelevanceTop1",
+        $"f_MatchedFpdRelevanceTop2",
+        $"f_MatchedFpdRelevanceTop3",
+        $"f_MatchedFpdRelevanceTop4",
+        $"f_MatchedFpdRelevanceTop5",
+        $"f_MatchedFpdRelevanceP50",
+        $"f_MatchedFpdRelevanceP90",
+        $"f_MatchedFpdRelevanceSum",
+        $"f_MatchedFpdRelevanceStddev",
+        $"f_MatchedFpdCount",
 
-        $"MatchedFpdRelevanceP50",
-        $"MatchedFpdRelevanceP90",
-        $"MatchedFpdRelevanceSum",
-        $"MatchedFpdRelevanceStddev",
-        $"MatchedFpdCount",
+        $"f_MatchedTpdRelevanceTop1",
+        $"f_MatchedTpdRelevanceTop2",
+        $"f_MatchedTpdRelevanceTop3",
+        $"f_MatchedTpdRelevanceTop4",
+        $"f_MatchedTpdRelevanceTop5",
 
-        $"MatchedTpdRelevanceP50",
-        $"MatchedTpdRelevanceP90",
-        $"MatchedTpdRelevanceSum",
-        $"MatchedTpdRelevanceStddev",
-        $"MatchedTpdCount",
+        $"f_MatchedTpdRelevanceP50",
+        $"f_MatchedTpdRelevanceP90",
+        $"f_MatchedTpdRelevanceSum",
+        $"f_MatchedTpdRelevanceStddev",
+        $"f_MatchedTpdCount",
 
-        $"MatchedFpdRelevanceP90xZipSiteLevel",
-        $"MatchedTpdRelevanceP90xZipSiteLevel",
+        $"f_MatchedExtendedFpdRelevanceTop1",
+        $"f_MatchedExtendedFpdRelevanceTop2",
+        $"f_MatchedExtendedFpdRelevanceTop3",
+        $"f_MatchedExtendedFpdRelevanceTop4",
+        $"f_MatchedExtendedFpdRelevanceTop5",
 
-        $"MatchedFpdRelevanceP90xOriginal_NeoScore",
-        $"MatchedTpdRelevanceP90xOriginal_NeoScore"
+        $"f_MatchedExtendedFpdRelevanceP50",
+        $"f_MatchedExtendedFpdRelevanceP90",
+        $"f_MatchedExtendedFpdRelevanceSum",
+        $"f_MatchedExtendedFpdRelevanceStddev",
+        $"f_MatchedExtendedFpdCount",
+
+        // $"MatchedFpdRelevanceP90xZipSiteLevel",
+        // $"MatchedTpdRelevanceP90xZipSiteLevel",
+
+        $"f_MatchedTpdRelevanceP90xOriginal_NeoScore",
+        $"f_MatchedFpdRelevanceP90xOriginal_NeoScore",
+        $"f_MatchedExtendedFpdRelevanceP90xOriginal_NeoScore",
+
+        $"f_Logit_NeoScore"
       )
       .collect()
 
@@ -109,26 +137,52 @@ class RelevanceBoostModelInputGeneratorTest extends TTDSparkTest {
           0.9f,
           "SeedId1",
           2,
-          "(1000,0.1),(1001,0.2),(1002,0.3),(1003,0.4),(1004,0.5),(1005,0.6),(1006,0.7),(1007,0.8),(1008,0.9),(1009,1.0),(2000,1.1),(2001,1.2),(2002,1.3),(2003,1.4),(2004,1.5),(2005,1.6),(2006,1.7),(2007,1.8),(2008,1.9),(2009,2.0)",
-          "(1000,0.1),(1001,0.2),(1002,0.3),(1003,0.4),(1004,0.5),(1005,0.6),(1006,0.7),(1007,0.8),(1008,0.9),(1009,1.0)",
+          "(2000,1.1),(2001,1.2),(2002,1.3),(2003,1.4),(2004,1.5),(2005,1.6),(2006,1.7),(2007,1.8),(2008,1.9),(2009,2.0)",
+          "(1000,0.1),(1001,0.2),(1002,0.3),(1003,0.4),(1004,0.5)",
+          "(1005,0.6),(1006,0.7),(1007,0.8),(1008,0.9),(1009,1.0)",
 
-          0.5f,
-          0.9f,
-          5.5f,
-          0.30276504f,
-          10,
 
-          1.5f,
-          1.9f,
-          15.5f,
-          0.30276504f,
-          10,
+          
+          0.40546509623527527f,	
+          0.33647221326828003f,	
+          0.2623642385005951f,	
+          0.18232159316539764f,	
+          0.09531020373106003f,	
+          0.0f,	
+          0.33647221326828003f,	
+          0.9162907600402832f,	
+          0.1467926800251007f,	
+          1.7917594909667969f,
 
-          1.8f,
-          3.8f,
 
-          0.80999994f,
-          1.7099999f
+          1.0986123085021973f,	
+          1.0647107362747192f,	
+          1.0296194553375244f,	
+          0.9932518005371094f,	
+          0.9555113911628723,
+
+          0.7419372797012329f,	
+          1.0647107362747192f,	
+          2.8033604621887207f,	
+          0.26448893547058105f,	
+          2.397895336151123,
+
+          0.6931471824645996f,
+          0.6418538689613342f,	
+          0.5877866148948669f,	
+          0.5306282639503479f,	
+          0.4700036346912384f,	
+          0.0f,	
+          0.5306282639503479f,	
+          1.8718022108078003f,	
+          0.26448896527290344f,	
+          2.397895336151123f,
+
+          0.9582396149635315f,
+          0.3028249740600586f,	
+          0.4775654375553131f,	
+          2.1972243785858154f
+     
         ),
       )
       )
